5fc461d958343f71e15d1792bd5199a707b27c66
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-03-25 14:29:32 +0200

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Ilya Tovstokory on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
